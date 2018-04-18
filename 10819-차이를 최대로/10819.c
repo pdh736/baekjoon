3ac16e948c93f837df72de7891f10027e9ec1a96
Daehee <pdh736@naver.com>
2018-04-18 09:27:22 +0900

    change

--- 10819-차이를 최대로/10819.c
+++ 10819-차이를 최대로/10819.c
@@ -1,42 +1,43 @@
 #include <stdio.h>
-
+#include <math.h>
 
 int next_permutation(int size);
-void sort(int size);
 int ary[8];
+
+int compare(const void *a, const void *b) {
+	return *(int*)a - *(int*)b;
+}
 
 int main() {
 	int n;
-	
-	int sum=0;
-	int p_sum=0;
+
+	int sum = 0;
+	int p_sum = 0;
 
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++)
 		scanf("%d", &ary[i]);
-	sort(n);
+	qsort(ary, n, sizeof(int), compare);
 
 	do {
 		p_sum = 0;
-		for (int i = 0; i < n-1; i = i ++)
-			p_sum += (ary[i] - ary[i+1]);
+		for (int i = 0; i < n - 1; i = i++)
+			p_sum += abs(ary[i] - ary[i + 1]);
 		if (sum < p_sum)sum = p_sum;
-	}
-	while (next_permutation(n));
-	
-	printf("%d\n", sum);
+	} while (next_permutation(n));
 
+	printf("%d\n", sum);
 
 	return 0;
 }
 
 int next_permutation(int size) {
-	int i,j;
+	int i, j;
 	int temp;
-	i = size-1;
+	i = size - 1;
 	while (i > 0 && (ary[i - 1] > ary[i])) i -= 1;
 	if (i <= 0)return 0;
-	j = size-1;
+	j = size - 1;
 	while (ary[j] < ary[i - 1])j -= 1;
 	temp = ary[j];
 	ary[j] = ary[i - 1];
@@ -50,15 +51,4 @@ int next_permutation(int size) {
 	}
 	return 1;
 }
-void sort(int size) {
-	int temp;
-	for (int i = 0; i < size; i++) {
-		for (int j = i + 1; j < size; j++) {
-			if (ary[i] > ary[j]) {
-				temp = ary[i];
-				ary[i] = ary[j];
-				ary[j] = temp;
-			}
-		}
-	}
-}
+
